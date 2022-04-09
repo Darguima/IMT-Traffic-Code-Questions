e62f09e20fbfa583c09e5bc104376f325a9478b8
Darguima <dsgdevbraga@gmail.com>
2022-04-09 14:48:15 +0100

    Not scraping images with big difference hash

--- scripts/modules/images_filter.py
+++ scripts/modules/images_filter.py
@@ -19,6 +19,7 @@ def images_filter (questions, scraped_images, base_url, images_output_dir):
 
 	images_hashes = {"A": {}, "B": {}, "C": {}, "D": {}}
 	necessary_images = []
+	big_difference_hash_images = []
 
 	for category in questions:
 		for question in questions[category]:
@@ -31,11 +32,12 @@ def images_filter (questions, scraped_images, base_url, images_output_dir):
 	print(f"\tCategory C - {len(images_hashes['C']):0>4}")
 	print(f"\tCategory D - {len(images_hashes['D']):0>4}")
 
-	print("\n\nComparing Hashes:")
+	print("\n\nComparing Hashes:", end="\r")
 	for category in images_hashes:
-		print(f"\tStarting category \"{category}\".")
+		print(f"\n\n\tStarting category \"{category}\":")
 
-		for image_id in images_hashes[category]:
+		for image_number, image_id in enumerate(images_hashes[category]):
+			print(f"\t\tImage num: {image_number:0>4}", end="\r")
 			if (exists(base_url)):
 				img = Image.open(f'{base_url}/imgs{category}/{image_id}.jpg')
 			
@@ -45,11 +47,19 @@ def images_filter (questions, scraped_images, base_url, images_output_dir):
 			
 			online_image_hash = dhash(img)
 
-			lower = 100
+			lower_dif_hash = 100
 			for scraped_img_hash in scraped_images:
-				if online_image_hash - scraped_img_hash < lower:
-					lower = online_image_hash - scraped_img_hash
-					images_hashes[category][image_id] = {"hash": str(scraped_img_hash), "error": lower}
+				if online_image_hash - scraped_img_hash < lower_dif_hash:
+					lower_dif_hash = online_image_hash - scraped_img_hash
+					images_hashes[category][image_id] = {"hash": str(scraped_img_hash), "error": lower_dif_hash}
+
+			if lower_dif_hash > 7:
+				img.save(f"{images_output_dir}/{str(online_image_hash)}.jpg", "JPEG")
+				images_hashes[category][image_id] = {"hash": str(online_image_hash), "error": -1}
+				big_difference_hash_images.append(str(online_image_hash))
+	
+	print(f"\n\nThis images diverges more than 7 bytes, so we are storing the online image with a black watermark: ({len(big_difference_hash_images)} images)")
+	print(", ".join(big_difference_hash_images))
 			
 	for category in questions:
 		for question in questions[category]:
@@ -66,4 +76,4 @@ def images_filter (questions, scraped_images, base_url, images_output_dir):
 			remove(f"{images_output_dir}/{str(image_hash)}.jpg")
 	
 	return questions
-		
+	
